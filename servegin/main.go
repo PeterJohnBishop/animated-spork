92663286c5c10c2a2dc6f5b984000ab22d09b807
PeterJohnBishop <peterjbishop.denver@gmail.com>
2026-08-08 19:48:32 -0600

    feat(api) basic gin server implemented

--- servegin/main.go
+++ servegin/main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Serving gin...")
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	router.Run(":8080")
 }
